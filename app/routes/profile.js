d458702d171fae792958fe7249a554f252ea84e1
Adolfo Builes <builes.adolfo@gmail.com>
2016-09-30 19:56:05 -0500

    Wire profile.

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin);
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  licSession: Ember.inject.service(),
+  model() {
+    return this.get('licSession.user');
+  }
+});
