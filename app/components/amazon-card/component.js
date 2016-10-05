7e36bd6117ff7c90c6e454a1a9db15d61905b147
Adolfo Builes <builes.adolfo@gmail.com>
2016-10-04 22:58:56 -0500

    Better cards.

--- app/components/amazon-card/component.js
+++ app/components/amazon-card/component.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['tc mb2'],
   init() {
     this._super(...arguments);
     this.set('payload', this.attrs.payload.value);
