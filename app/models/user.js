231534a42727ef22567051ecc67f2f38483c9af7
Adolfo Builes <builes.adolfo@gmail.com>
2016-10-06 00:25:34 -0500

    Display username.

--- app/models/user.js
+++ app/models/user.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   annotations: DS.hasMany('annotation'),
-  annotationsCount: DS.attr('number')
+  annotationsCount: DS.attr('number'),
+  username: DS.attr('string'),
+  slug: DS.attr('string')
 });
