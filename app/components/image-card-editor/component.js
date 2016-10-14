b732b027cc12d1d5af315e81c1faede114c4966c
Adolfo Builes <builes.adolfo@gmail.com>
2016-10-14 13:48:35 -0500

    Use cps to get image attrs.

--- app/components/image-card-editor/component.js
+++ app/components/image-card-editor/component.js
@@ -1,12 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  init() {
-    this._super(...arguments);
-    this.set('src', this.get('attrs.payload.value.src') || '');
-    this.set('title', this.get('attrs.payload.value.title') || '');
-    this.set('alt', this.get('attrs.payload.value.alt') || '');
-  },
+  src: Ember.computed('payload.src', {
+    get() {
+      return this.get('payload.src') || '';
+    }
+  }).readOnly(),
+  title: Ember.computed('payload.title', {
+    get() {
+      return this.get('payload.title') || '';
+    }
+  }).readOnly(),
+  alt: Ember.computed('payload.alt', {
+    get() {
+      return this.get('payload.alt') || '';
+    }
+  }).readOnly(),
   save(src, title, alt ) {
     if (src) {
       this.saveCard({src, title, alt});
--- app/components/image-card/component.js
+++ app/components/image-card/component.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  init() {
-    this._super(...arguments);
-    this.set('src', this.get('attrs.payload.value.src') || '');
-    this.set('title', this.get('attrs.payload.value.title') || '');
-    this.set('alt', this.get('attrs.payload.value.alt') || '');
-  },
+  src:   Ember.computed.readOnly('payload.src'),
+  title: Ember.computed('payload.title', {
+    get() {
+      return this.get('payload.title') || '';
+    }
+  }).readOnly(),
+  alt: Ember.computed('payload.alt', {
+    get() {
+      return this.get('payload.alt') || '';
+    }
+  }).readOnly(),
   doubleClick() {
     this.attrs.editCard(this.attrs.payload);
   }
